7fabf8517479ff89659e2744c7c58705badfa9a9
jurio5 <sia5492@naver.com>
2025-06-16 00:59:48 +0900

    [feat] 가장 순수한 JPA 방식

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -10,7 +10,20 @@ public class JpaMain {
         EntityManager em = emf.createEntityManager();
         //code
 
-        em.close();
-        emf.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setName("HelloA");
+
+            em.persist(member);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+            emf.close();
+        }
     }
 }
