4fea5765fe452cab89681f5b0c4ed3692b326841
jurio5 <sia5492@naver.com>
2025-06-16 21:13:51 +0900

    [feat] 영속성 컨텍스트에 저장(persist)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,8 +17,12 @@ public class JpaMain {
         tx.begin();
 
         try {
+            // 비영속
             Member member = new Member();
             member.setName("HelloJpa");
+
+            // 영속
+            em.persist(member); // 이 때 영속성 컨텍스트(1차 캐시)에 영속화
 
             tx.commit();
         } catch (Exception e) {
